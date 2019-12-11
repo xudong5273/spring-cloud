5258c6ae1f0f5a68c0bf7afa602e1552861f466e
chenxd <chenxd@rayeye.cn>
2019-12-11 16:39:05 +0800

    配置中心bug解决

--- config/src/main/java/com/beita/config/ConfigApplication.java
+++ config/src/main/java/com/beita/config/ConfigApplication.java
@@ -1,13 +1,13 @@
-package com.beita.eurekaclienttest;
+package com.beita.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EurekaClientTestApplication {
+public class ConfigApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaClientTestApplication.class, args);
+        SpringApplication.run(ConfigApplication.class, args);
     }
 
 }
