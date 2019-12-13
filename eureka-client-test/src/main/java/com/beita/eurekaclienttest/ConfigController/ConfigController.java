1595cf98bf1f260ac1d2ce88ad07ff097405fbdb
chenxd <chenxd@rayeye.cn>
2019-12-13 18:22:55 +0800

    消息总线+动态更新配置

--- eureka-client-test/src/main/java/com/beita/eurekaclienttest/ConfigController/ConfigController.java
+++ eureka-client-test/src/main/java/com/beita/eurekaclienttest/ConfigController/ConfigController.java
@@ -1,9 +1,16 @@
 package com.beita.eurekaclienttest.ConfigController;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+    http://localhost:8881/actuator/bus-refresh
+    http://localhost:8881/actuator/bus-refresh?destination=customers:**
+    /actuator/bus-refresh接口可以指定服务，即使用"destination"参数，比如 “/actuator/bus-refresh?destination=customers:**” 即刷新服务名为customers的所有服务
+ */
+@RefreshScope
 @RestController
 public class ConfigController {
 
